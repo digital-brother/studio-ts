551544be54295bb184485c132b4951d82d4f7584
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-06-08 23:11:15 +0300

    Update Header logo, Footer, home hero - fix company name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -186,7 +186,7 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-balance text-neutral-950 sm:text-7xl">
-            Award-winning development studio based in Denmark.
+            Award-winning development studio based in Ukraine.
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
             We are a development studio working at the intersection of design
