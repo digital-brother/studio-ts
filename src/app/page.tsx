f58e9e6d9e31d69a429693e5e971007660771f14
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-06-08 23:19:45 +0300

    Change compant name in feedback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -204,7 +204,7 @@ export default async function Home() {
         className="mt-24 sm:mt-32 lg:mt-40"
         client={{ name: 'Phobia', logo: logoPhobiaDark }}
       >
-        The team at Studio went above and beyond with our onboarding, even
+        The team at Digital Brother went above and beyond with our onboarding, even
         finding a way to access the user’s microphone without triggering one of
         those annoying permission dialogs.
       </Testimonial>
